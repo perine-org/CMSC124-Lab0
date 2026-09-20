84c346e20afe636cc2e6e7e71110dfbd2cb3acec
Percie Louise Y. Samaniego <pysamaniego1@up.edu.ph>
2026-09-21 01:47:49 +0800

    feat: equality parsing for Equal and Not Equal, also changed parse_expression from factor() to equality()

--- src/parser.rs
+++ src/parser.rs
@@ -20,7 +20,7 @@ impl Parser {
     }
 
     pub fn parse_expression(&mut self) -> Expr {
-        self.factor()
+        self.equality()
     }
 
     fn term(&mut self) -> Expr {
@@ -43,6 +43,16 @@ impl Parser {
             let operator = self.previous();
             let right = self.term();
             expr = Expr::Binary {left: Box::new(expr), operator, right: Box::new(right)};
+        }
+        expr
+    }
+
+    fn equality(&mut self) -> Expr {
+        let mut expr = self.comparison();
+        while self.match_types(&[TokenType::Equal, TokenType::NotEqual]) {
+            let operator = self.previous();
+            let right = self.comparison();
+            expr = Expr::Binary { left: Box::new(expr), operator, right: Box::new(right) };
         }
         expr
     }
