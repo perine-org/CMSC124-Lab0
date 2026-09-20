1b04065118bd80b8412c7fdd286b94b9767b1d10
Percie Louise Y. Samaniego <pysamaniego1@up.edu.ph>
2026-09-21 02:19:56 +0800

    feat: add display for expr to print the parse tree

--- src/ast.rs
+++ src/ast.rs
@@ -1,4 +1,5 @@
 use crate::token::{Literal, Token};
+use std::fmt;
 
 #[derive(Debug, Clone)]
 pub enum Expr {
@@ -13,4 +14,16 @@ pub enum Expr {
         operator: Token,
         right: Box<Expr>,
     },
+}
+
+impl fmt::Display for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Expr::Literal(lit) => write!(f, "{}", lit),
+            Expr::Grouping { expression } => write!(f, "(group {})", expression),
+            Expr::Binary { left, operator, right } => {
+                write!(f, "({} {} {})", operator.lexeme, left, right)
+            }
+        }
+    }
 }
