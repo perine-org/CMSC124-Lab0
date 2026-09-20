0f32cf2bad5117a3029436349b8e51bd8bdfe942
e-rine <elmedalla@up.edu.ph>
2026-09-20 22:17:53 +0800

    feat: add Literal and Binary AST nodes

---
+++ src/ast.rs
@@ -0,0 +1,12 @@
+use crate::token::{Literal as TokenLiteral, Token};
+
+#[derive(Debug, Clone)]
+pub enum Expr {
+    Literal(TokenLiteral),
+
+    Binary {
+        left: Box<Expr>,
+        operator: Token,
+        right: Box<Expr>,
+    },
+}
